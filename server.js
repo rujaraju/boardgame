c28b2e70e898f7776e862bfb8c48ad6d0086a3e8
Ubuntu <ubuntu@ip-172-31-46-193.eu-west-1.compute.internal>
2020-10-22 12:43:49 +0000

    port

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 
 var express = require('express')
 var app = express()
+var port = process.env.PORT || 8080;
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/app', express.static(process.cwd() + '/app'));
 app.use(express.static(process.cwd() + '/public'))
 
-app.listen(8080, function(){console.log("Server listening on port 8080")});
+app.listen(port, function(){console.log("Server listening on port 8080")});
