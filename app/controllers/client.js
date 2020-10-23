a204103c0ed520bb59069fcfe026e2d27b503dcf
Ubuntu <ubuntu@ip-172-31-46-193.eu-west-1.compute.internal>
2020-10-23 09:17:26 +0000

    test touchmove console

--- app/controllers/client.js
+++ app/controllers/client.js
@@ -23,9 +23,10 @@ $("#board").mousemove(function(event){
 if (isTouchScreen){
     $("#board").on('touchmove', function(event){ 
             event.preventDefault()
-            alert("anything")
+            //alert("anything")
             relX = Math.round(event.pageX - $(this).offset().left);
             relY = Math.round(event.pageY - $(this).offset().top);
+            console.log(relX, relY)
             $("#"+toBeMoved).css("top", relY-7 +"px")
             $("#"+toBeMoved).css("left", relX-7 +"px")
             $("#"+toBeMoved + "overlay").css("top", relY-7 +"px")
