5d944e0d404e421e1b50eb08255dea261013b4c4
Ubuntu <ubuntu@ip-172-31-46-193.eu-west-1.compute.internal>
2020-10-23 09:07:25 +0000

    test touchmove

--- app/controllers/client.js
+++ app/controllers/client.js
@@ -2,38 +2,40 @@ var relX
 var relY
 var toBeMoved;
 var highestZ = 0
-
-function is_touch_enabled() { 
-    return ( 'ontouchstart' in window ) ||  
-           ( navigator.maxTouchPoints > 0 ) ||  
-           ( navigator.msMaxTouchPoints > 0 ); 
-}
-
+ 
 function checkTouchDevice() {
    return 'ontouchstart' in document.documentElement;
 }
 
-alert(is_touch_enabled())
-alert(checkTouchDevice())
+var isTouchScreen = checkTouchDevice()
 
-$("#board").mousemove(function(event){            
+$("#board").mousemove(function(event){ 
+            event.preventDefault()
             relX = Math.round(event.pageX - $(this).offset().left);
             relY = Math.round(event.pageY - $(this).offset().top);
             var relBoxCoords = "(" + relX + "," + relY + ")";
-            //$("#redpiece").attr("top", relY +"px")
             $("#"+toBeMoved).css("top", relY-7 +"px")
             $("#"+toBeMoved).css("left", relX-7 +"px")
             $("#"+toBeMoved + "overlay").css("top", relY-7 +"px")
             $("#"+toBeMoved + "overlay").css("left", relX-7 +"px")
-            //console.log(relBoxCoords);
-            //console.log($("#redpiece").attr("top"))
-        });
-        
-        function test(){
-            test3(event.target.id)
-}
 
-        function test3(id){
+        });
+
+if (isTouchScreen){
+    $("#board").on('touchmove', function(event){ 
+            event.preventDefault()
+            relX = Math.round(event.pageX - $(this).offset().left);
+            relY = Math.round(event.pageY - $(this).offset().top);
+            var relBoxCoords = "(" + relX + "," + relY + ")";
+            $("#"+toBeMoved).css("top", relY-7 +"px")
+            $("#"+toBeMoved).css("left", relX-7 +"px")
+            $("#"+toBeMoved + "overlay").css("top", relY-7 +"px")
+            $("#"+toBeMoved + "overlay").css("left", relX-7 +"px")
+
+        });
+}
+        
+function move(id){
             console.log(id)
             if (id.length < 3){return}
             if (toBeMoved == undefined){
@@ -46,18 +48,11 @@ $("#board").mousemove(function(event){
             }
 }
 
-        function test2(){
-            test3(event.target.parentElement.id)
+        function moveIt(){
+            move(event.target.id.split("overl")[0])
 }
 
-        function test4(){
-            test3(event.target.id.split("overl")[0])
-}
 
-function pause(milliseconds) {
-	var dt = new Date();
-	while ((new Date()) - dt <= milliseconds) { /* Do nothing */ }
-}
 
 function kasta(){
     console.log("working")
