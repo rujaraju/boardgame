ae4a70153e5f3a3c406132d09405b420d303aa53
Ubuntu <ubuntu@ip-172-31-46-193.eu-west-1.compute.internal>
2020-10-26 13:57:40 +0000

    no shit

--- server.js
+++ server.js
@@ -12,4 +12,21 @@ app.use(express.static(process.cwd() + '/public'))
 
 const server = http.createServer(app)
 
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log("anything")
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
+
 server.listen(port, function(){console.log("Server listening on port 8080")});
